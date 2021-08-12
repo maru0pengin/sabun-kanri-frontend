43bb681d0700f843bfe2f973a95243998965015b
maru0pengin <maru0pengin@gmail.com>
2021-08-12 18:37:23 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<{},{ text: string }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  componentDidMount() {
+    const fetchInit = {
+      method: "GET",
+      headers: { "content-type": "application/json" }
+    };
+    let url:string = new URL("hello_world", process.env.REACT_APP_SERVER_URL).toString()
+    fetch(url, fetchInit)
+      .then(response => response.json())
+      .then(response => this.setState(response));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Response: {this.state.text}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
