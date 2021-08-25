4ce3eb9f6267d00aa643c957c64e47c57bb60940
maru0pengin <maru0pengin@gmail.com>
2021-08-26 00:07:45 +0900

    update:psd保存を試し中

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import { DropDirectory } from './components/DropDirectory'
 
 const PSD = require('psd.js')
 
-class App extends Component<{}, { text: string, layers: {url: string,top: string,left: string}[], canvasWidth:number }> {
+class App extends Component<{}, { text: string, layers: {url: string,top: string,left: string}[], canvasWidth:number,psd:any }> {
   constructor(props: any) {
     super(props);
 
     this.state = {
       text: '',
       layers: [],
-      canvasWidth: 0
+      canvasWidth: 0,
+      psd: null
     };
   }
-
   async componentDidMount() {
     const fetchInit = {
       method: "GET",
@@ -39,8 +39,9 @@ class App extends Component<{}, { text: string, layers: {url: string,top: string
   }
 
   handleDrop = async(e: any) => {
-
     let psd = await PSD.fromEvent(e)
+    console.log(e)
+    this.setState({ psd :e.target.files })
     let layerNum = psd.tree().descendants().length
     let canvasWidth = psd.header.cols
     for (let i = layerNum-1; i >= 0; i--){
@@ -53,6 +54,45 @@ class App extends Component<{}, { text: string, layers: {url: string,top: string
     e.preventDefault()
   }
 
+  sendPsd = async (e: any) => {
+    // await fetch(`http://localhost:5000/api/psd/3`, {
+    //   method:'DELETE',
+    //   mode: 'cors'
+    // })
+
+    var formData = new FormData()
+    formData.append('title', 'testたいとる')
+    formData.append('psd', this.state.psd)
+    //formData.append("psd", '')
+    console.log(formData)
+    const res = await fetch(`http://localhost:5000/api/psd`, {
+      method:'POST',
+      // headers: {
+      //   "Content-Type": "multipart/form-data"
+      // },
+      body: formData
+    })
+
+    // if (!res.ok) {
+    //   let err
+    //   try {
+    //     err = await res.json()
+    //   } catch {
+    //     err = {
+    //       statusCode: res.status,
+    //       message: res.statusText,
+    //     }
+    //   }
+    //   console.log(err)
+    // }
+    console.log(res)
+  }
+
+  setImage = (e:any) => {
+    e.preventDefault();
+    this.setState({ psd :e.target.files[0]})
+  }
+
   render() {
 
     return (
@@ -60,6 +100,7 @@ class App extends Component<{}, { text: string, layers: {url: string,top: string
         <header className="App-header">
           
           <div onDragOver={this.handleDragOver} onDrop={this.handleDrop}>ここへPSDをドロップ</div>
+          <button onClick={this.sendPsd}>保存</button>
           <div style={{ position: "relative" ,backgroundColor: "red" ,marginTop: "50px", width: this.state.canvasWidth}}>
             {/* <img src={this.state.imgSrc3} style={{ position: "absolute", top:this.state.top, left:this.state.left}}/>
             <img src={this.state.imgSrc2} style={{ position: "absolute" }}/>
@@ -68,7 +109,7 @@ class App extends Component<{}, { text: string, layers: {url: string,top: string
               return <img src={layer?.url} style={{ position: "absolute", top:layer?.top, left:layer?.left, maxWidth: "500px"}}/>
             })}
           </div>
-
+          <input type="file" id="image" name="image"  onChange={this.setImage} />
           {/* <DropZone />
           <DropDirectory/> */}
         </header>
