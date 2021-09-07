24c838a06a1564f727167e196b87d14d2382d34c
maru0pengin <maru0pengin@gmail.com>
2021-09-08 00:22:47 +0900

    update:全パターン表示を仮完成

--- src/views/components/SabunList.tsx
+++ src/views/components/SabunList.tsx
@@ -34,29 +34,44 @@ const SabunList: React.FC<SabunListProps> = (props) => {
     let _list:LayerType[][] = list
     let _layers: LayerType[] = []
 
+    let loopIndexList = []
+    for (let i = 0; i < 7; i++) {
+      loopIndexList[i] = psd.tree().children()[i]?.children().length
+      if (loopIndexList[i] === undefined || loopIndexList[i] === 0) loopIndexList[i] = 1
+    }
+
+    console.log(loopIndexList)
+
     console.log(layerNum)
     let layer
-    for (let s = 0; s < 3; s++) {
-      for (let k = 0; k < 3; k++) {
-        for (let j = 0; j < 3; j++) {
-          for (let i = layerNum - 1; i >= 0; i--) {
-            layer = {}
-            if (psd.tree().children()[i]._children.length > 0) {
-              console.log(`j:${j}`)
-              if (i === 0) layer = psd.tree().children()[i].children()[j].layer
-              else if (i === 1) layer = psd.tree().children()[i].children()[k].layer
-              else if (i === 2) layer = psd.tree().children()[i].children()[s].layer
-              else layer = psd.tree().children()[i].children()[0].layer
-            } else {
-              console.log(`i:${i}`)
-              layer = psd.tree().children()[i].layer
+    // このプログラムでは5項目の差分までしか表示できない
+    // ToDo:簡潔なコードへ修正する
+    console.log(loopIndexList[4])
+    for (let n = 0; n < loopIndexList[4]; n++) {
+      for (let m = 0; m < loopIndexList[3]; m++) {
+        for (let l = 0; l < loopIndexList[2]; l++) {
+          for (let k = 0; k < loopIndexList[1]; k++) {
+            for (let j = 0; j < loopIndexList[0]; j++) {
+              for (let i = layerNum - 1; i >= 0; i--) {
+                layer = {}
+                if (psd.tree().children()[i]._children.length > 0) {
+                  if (i === 0) layer = psd.tree().children()[i].children()[j].layer
+                  else if (i === 1) layer = psd.tree().children()[i].children()[k].layer
+                  else if (i === 2) layer = psd.tree().children()[i].children()[l].layer
+                  else if (i === 3) layer = psd.tree().children()[i].children()[m].layer
+                  else if (i === 4) layer = psd.tree().children()[i].children()[n].layer
+                  else layer = psd.tree().children()[i].children()[0].layer
+                } else {
+                  layer = psd.tree().children()[i].layer
+                }
+                let url = layer.image.toBase64()
+                _layers = _layers.concat({ url: url, top: layer.top / scale, left: layer.left / scale, width: layer.width / scale })
+              }
+              _list.push(_layers)
+              //_list[j ] = _layers
+              _layers = []
             }
-            let url = layer.image.toBase64()
-            _layers = _layers.concat({ url: url, top: layer.top / scale, left: layer.left / scale, width: layer.width / scale })
           }
-          _list.push(_layers)
-          //_list[j ] = _layers
-          _layers = []
         }
       }
     }
