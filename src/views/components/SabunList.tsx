3d5ca79d2bdbf46d86ba7d89450ee2b7aabfb9d4
maru0pengin <maru0pengin@gmail.com>
2021-09-07 21:54:06 +0900

    update:一覧では無いが差分を表示

--- src/views/components/SabunList.tsx
+++ src/views/components/SabunList.tsx
@@ -16,70 +16,51 @@ interface LayerType {
 
 const SabunList: React.FC<SabunListProps> = (props) => {
   const [list, setList] = useState<LayerType[][]>([[]])
-  const [layers, setlayers] = useState<LayerType[]>([])
-  const [canvasSize, setCanvasSize] = useState<{ width: number, height: number }>({width:0,height:0})
-  const [test, setTest] = useState<string[]>(["a","b"])
-  let scale:number = 1
+  const [canvasSize, setCanvasSize] = useState<{ width: number, height: number }>({ width: 0, height: 0 })
+  
+  const [update, setUpdata] = useState<boolean>(false)
+  
+  let scale: number = 1
+  const width = 300
 
-  const showList = async() => {
+  console.log("再レンダリング")
+
+  const showList = async () => {
     let psd = await PSD.fromURL(props.psdUrl)
     let layerNum = psd.tree().children().length
-    scale = psd.header.cols / 300
+    scale = psd.header.cols / width
     let _canvasSize = { width: psd.header.cols / scale, height: psd.header.rows / scale }
-    let _list:LayerType[][] = list
-    let _layers:LayerType[] = []
-    for (let i = layerNum - 1; i >= 0; i--) {
-      let layer
-      if (psd.tree().children()[i]._children.length > 0) {
-        //console.log(`下から${i + 1}番目の要素はディレクトリです`)
-        layer = psd.tree().children()[i].children()[0].layer
-      } else {
-        layer = psd.tree().children()[i].layer
-        //console.log(`下から${i + 1}番目の要素はレイヤーです`)
-      }
-      let url = layer.image.toBase64()
-      _layers = _layers.concat({ url: url, top: layer.top/scale, left: layer.left/scale, width: layer.width/scale })
-    }
-    //setlayers(_layers)
     setCanvasSize(_canvasSize)
-
+    let _list:LayerType[][] = list
+    let _layers: LayerType[] = []
+    for (let j = 0; j < 3; j++) {
+      for (let i = layerNum - 1; i >= 0; i--) {
+        let layer
+        if (psd.tree().children()[i]._children.length > 0) {
+          layer = psd.tree().children()[i].children()[j].layer
+        } else {
+          layer = psd.tree().children()[i].layer
+        }
+        let url = layer.image.toBase64()
+        _layers = _layers.concat({ url: url, top: layer.top / scale, left: layer.left / scale, width: layer.width / scale })
+      }
+      _list[j] = _layers
+    }
     
-    _list[0] = _layers
-    console.log(`_layers${_layers}`)
-    console.log(_list)
     setList(_list)
+    console.log("うに丼")
+    console.log(_list)
+    //ToDo: 強制再レンダリングをしない方法を調べる
+    setUpdata(update?false:true)
   }
   return (
     <div>
       <button onClick={showList}>リスト表示</button>
-      {/* <div style={{ position: "relative", backgroundColor: "red", marginTop: "50px", width: canvasSize.width, height: canvasSize.height }}>
-        test
-        {layers.map((layer,i) => {
-          return <img width={layer.width/scale} key={i} src={layer?.url} style={{ position: "absolute", top:layer?.top, left:layer?.left, maxWidth: "500px"}}/>
-        })}
-      </div>
-       */}
-      {/* {list.map((layers, i) => {
-        return (
-          <div style={{ position: "relative", backgroundColor: "red", marginTop: "50px", width: canvasSize.width, height: canvasSize.height }}>
-            { layers.map((layer, j) => {
-              return (
-                test < img width = { layer.width / scale } key = { i * j} src = { layer?.url
-              } style = {{ position: "absolute", top: layer?.top, left: layer?.left, maxWidth: "500px" }} />
-            )}
-          </div>
-        )
-      }
-        )
-      })} */}
-      {/* {test.map((test,i) => (
-          <div>{test}</div>
-      ))} */}
 
       {list.map((layers, i) => {
         return (<div style={{ position: "relative", backgroundColor: "red", marginTop: "50px", width: canvasSize.width, height: canvasSize.height }}>{layers.map((layer,j) => {
           return (
-            < img width = { layer.width / scale } key = { i * j} src = { layer?.url
+            < img width = { layer.width / scale } key = {`${i}+${j}`} src = { layer?.url
             } style = {{ position: "absolute", top: layer?.top, left: layer?.left, maxWidth: "500px" }} />
         )})}</div>)
       })}
